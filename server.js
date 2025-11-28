12a9ff93b83e47ab398c1bec8ee868e81973fb4c
Fors-ony <onynweke02@gmail.com>
2025-11-28 12:48:57 +0000

    add auth routes to server.js

--- server.js
+++ server.js
@@ -4,7 +4,8 @@ const cors = require('cors');
 // const mongoose = require('mongoose');
 
 const app = express();
-
+const authRoutes = require('./routes/auth');
+app.use('/api/auth', authRoutes);
 app.use(cors());
 app.use(express.json({ limit: '10mb' })); // Accept base64 images
 
