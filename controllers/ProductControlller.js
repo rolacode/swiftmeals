df3011f7b53566f02dbb9a97d6f45661b4d97feb
Fors-ony <onynweke02@gmail.com>
2025-12-03 07:25:59 +0000

    add ProductController.js

--- controllers/ProductControlller.js
+++ controllers/ProductControlller.js
@@ -1,1 +1,30 @@
 
+const Product = require('../models/Product');
+
+exports.createProduct = async (req, res) => {
+  try {
+    const { name, price, category, description, image } = req.body;
+
+    const newProduct = await Product.create({
+      name,
+      price,
+      category,
+      description,
+      image,
+      createdBy: req.user.id
+    });
+
+    res.status(201).json({ message: "Product created", product: newProduct });
+  } catch (error) {
+    res.status(500).json({ message: "Server error", error });
+  }
+};
+
+exports.getProducts = async (req, res) => {
+  try {
+    const products = await Product.find();
+    res.json(products);
+  } catch (error) {
+    res.status(500).json({ message: "Server error", error });
+  }
+};
