bb3775bdb28dfab4185393978751b2897ba4d16e
Fors-ony <onynweke02@gmail.com>
2025-12-03 07:26:58 +0000

    add product.js

--- routes/product.js
+++ routes/product.js
@@ -1,1 +1,10 @@
 
+const express = require('express');
+const router = express.Router();
+const { createProduct, getProducts } = require('../controllers/productController');
+const authMiddleware = require('../middleware/authMiddleware');
+
+router.post('/add', authMiddleware, createProduct);
+router.get('/', getProducts);
+
+module.exports = router;
