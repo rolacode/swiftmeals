28764a9e311e3f4e631251f4a9130ac59dd6f5f4
Fors-ony <onynweke02@gmail.com>
2025-12-03 07:24:28 +0000

    add Product.js

--- models/Product.js
+++ models/Product.js
@@ -1,1 +1,16 @@
 
+const mongoose = require('mongoose');
+
+const productSchema = new mongoose.Schema({
+  name: { type: String, required: true },
+  description: String,
+  price: { type: Number, required: true },
+  category: { type: String, required: true },
+  image: String, // image URL
+  createdBy: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "User",
+  }
+}, { timestamps: true });
+
+module.exports = mongoose.model('Product', productSchema);
